2f73c8a331bb8344bd967708c4fbd6b8e9c58609
Gianpaolo Terranova <gianpaoloterranova@gmail.com>
2019-12-16 01:11:11 +0100

    Initial commit

---
+++ src/api.ts
@@ -0,0 +1,42 @@
+import { Router } from 'express';
+
+export const apiRoutes = config => {
+  var apiRouter = Router();
+  const { dispatcher } = config;
+
+  // Example Express Rest API endpoints
+  apiRouter.use('', (req, res, next) => {
+    const action = {
+      GET: 'get',
+      POST: 'push',
+      PUT: 'set',
+      PATCH: 'update',
+      DELETE: 'del'
+    }[req.method];
+    const uri = decodeURI(req.path.slice(1));
+    let fn;
+    if (req.query.schema) {
+      fn = dispatcher.getSchema(uri);
+    } else {
+      fn = dispatcher.dispatch(action, uri, undefined, req.body);
+    }
+    console.log(action, uri)
+    try {
+      return fn.then(data => {
+        if (!data) {
+          res.statusCode = 404;
+          res.send();
+        } else {
+          res.json(data)
+        }
+      }).catch(reason => {
+        res.statusCode = 400;
+        res.send(reason);
+      });  
+    } catch {
+      res.statusCode = 500;
+      res.send({ error: "Server error"});
+    }
+  });
+  return apiRouter;
+};
---
+++ src/server.ts
@@ -0,0 +1,119 @@
+import * as express from 'express';
+import { join } from 'path';
+
+import * as bodyParser from 'body-parser';
+import * as logger from 'morgan';
+import * as cors from 'cors';
+//import * as expressJwt from 'express-jwt';
+//import { adminRoutes } from './admin';
+//import { authRoutes } from './auth';
+import { createDatasource } from './datasource';
+import { apiRoutes } from './api';
+import { AutoInquirer, PromptBuilder } from 'autoinquirer';
+
+//import { uploadRoutes } from './upload';
+//import { contactsRoutes } from './contacts';
+//import { generateSitemap } from './sitemap';
+import * as SocketIO from 'socket.io';
+
+var program = require('commander');
+
+const config = { secret: 'my-secret', dispatcher: null, autoinquirer: null };
+
+// Express server
+async function main() { // jshint ignore:line
+  // jshint ignore:line
+  const app = express();
+  const http = require('http');
+  const server = http.createServer(app) // io requires raw http
+  const io = SocketIO(server) // Setup Socket.io's server
+  const PORT = process.env.PORT || 4000;
+  const DIST_FOLDER = join(process.cwd(), 'dist');
+
+  config.dispatcher = await createDatasource(
+    // jshint ignore:line
+    join(program.args[0]),
+    join(program.args[1]),
+    new PromptBuilder()
+  );
+ 
+  const autoinquirer = new AutoInquirer(config.dispatcher);
+
+  app.use(
+    cors({
+      credentials: true
+    })
+  );
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(logger('dev'));
+  //app.use(
+  //  expressJwt({
+  //    secret: config.secret,
+  //    credentialsRequired: false
+  //  })
+  //);
+
+  // Catch all other routes and return the index file
+  //app.use('/admin', adminRoutes(ADMIN_FOLDER));
+  //app.use('/auth', authRoutes(config));
+  //app.use('/contacts', contactsRoutes());
+
+  // Example Express Rest API endpoints
+  //app.use('/api/files', uploadRoutes(UPLOAD_FOLDER));
+  app.use('/api', apiRoutes(config));
+
+  // Server static files from /browser
+  app.get(
+    '*.*',
+    express.static(DIST_FOLDER, {
+      maxAge: '1y'
+    })
+  );
+
+  // All regular routes use the Universal engine
+  //app.get('*', (req, res) => {
+  //  res.render('index', { req });
+  //});
+
+  io.on("connection", (socket) => {
+    autoinquirer.on('prompt', prompt => {
+        //console.log('Prompt: ' + prompt);
+        socket.broadcast.emit("prompt", prompt);
+    });
+    autoinquirer.on('error', state => { 
+        socket.broadcast.emit('error', state)
+    });
+    autoinquirer.on('exit', state => socket.broadcast.emit('exit', state));
+    autoinquirer.on('complete', () => config.dispatcher.close() );
+    socket.on('answer', (data) => {
+      //console.log('Answer: ' + JSON.stringify(data));
+      autoinquirer.onAnswer(data).then(() => autoinquirer.run());
+    });
+    autoinquirer.run();
+  });
+  
+  // Start up the Node server
+  server.listen(PORT, () => {
+    console.log(`Node Express server listening on http://localhost:${PORT}`);
+  });
+
+  process.on('unhandledRejection', (err, p) => {
+    console.log('An unhandledRejection occurred');
+    console.log(`Rejected Promise: ${p}`);
+    console.log(`Rejection:`, err);
+    // dispatcher.close();
+  });
+}
+
+program
+  .version('1.0.0')
+  .description('Example json editor')
+  .arguments('<schemaFile> <dataFile>')
+  .parse(process.argv);
+
+if (program.args.length < 1) {
+    program.outputHelp();
+} else {
+    main();
+}
