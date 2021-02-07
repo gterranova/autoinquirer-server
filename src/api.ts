40f9c596bda7a55eadb7aba73c984c7c57ea1d26
Gianpaolo Terranova <gianpaoloterranova@gmail.com>
2021-02-07 13:05:45 +0100

    Added api/json-rpc entrypoint

--- src/api.ts
+++ src/api.ts
@@ -28,7 +28,7 @@ export const apiRoutes = (renderer: Dispatcher) => {
       maxFields: 1000, // Max files & fields - default = 1000
       hash: false, // sha1, md5 or false
     });
-    console.log(req.method.toLowerCase(), req.body);
+    //console.log(req.method.toLowerCase(), req.body);
     (<any>req).files = await new Promise((resolve, reject) => {
       form.parse(req, (err, fields, files) => {
         if (err) {
@@ -42,6 +42,38 @@ export const apiRoutes = (renderer: Dispatcher) => {
       return res.send(reason);
     });
     return next();
+  });
+
+  apiRouter.post('/json-rpc' , async (req, res, next) => {
+    const { jsonrpc, method, params, id } = req.body;
+    if (jsonrpc !== '2.0' || !method || !params?.path || !id) {
+      return res.json({jsonrpc: '2.0', id, error: { code: 400, message: 'Malformed RPC request' }});
+    }
+    const uri = decodeURI(params.path.slice(1));
+    const user = (<any>req).user?.uid ? await renderer.dispatch(Action.GET, <IDispatchOptions>{ itemPath: `auth/users/${(<any>req).user.uid}` }) : null;
+    const reqTransformer = req.query['do'] && Array.isArray(req.query['do']) ? req.query['do'][0] : req.query['do'];
+    const transformer = renderer.getTransformer(reqTransformer) || renderer.dispatch.bind(renderer);
+    const value = (Object.keys(params.value || {}).length>0 && params.value) || undefined;
+    const files = (<any>req).files;
+    const fn = transformer(method, <IDispatchOptions>{ itemPath: uri, value, query: req.query, files, user });
+    const responseObj = {jsonrpc, id };
+    try {
+      return fn.then((data: any)=> {
+        if (data === undefined || data === null) {
+          res.statusCode = 404;
+          res.json({...responseObj, error: { code: res.statusCode, message: 'Resource not found' }});
+        } else {
+          res.json({...responseObj, result: data })
+        }
+      }).catch((reason: any) => {
+        console.error(reason);
+        res.statusCode = 400;
+        res.json({...responseObj, error: { code: res.statusCode, message: reason }});
+      });  
+    } catch {
+      res.statusCode = 500;
+      res.json({...responseObj, error: { code: res.statusCode, message: 'Server Error' }});
+    }
   });
 
   // Example Express Rest API endpoints
